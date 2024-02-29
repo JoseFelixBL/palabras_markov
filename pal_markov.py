4245ad853d01336f362414227e5137a053192705
José Bello <jfbello.work@gmail.com>
2024-02-29 13:13:57 +0100

    Refactoring en funciones
    leer_texto(texto)
    elegir_palabra(palabras)
    escibir_grafo(inicio)

--- pal_markov.py
+++ pal_markov.py
@@ -1,3 +1,5 @@
+import random
+
 """Palabras de Markov"""
 
 """Viene de https://www.youtube.com/watch?v=8ext9G7xspg&t=9109s
@@ -11,63 +13,78 @@
 """
 
 
-import random
-texto = "I am, subscribed to Y-Cubed and I am 123 loving678 it."
-texto = "I am, subscribed to Y Cubed and I am 123 loving678 it."
-# Quitar los separadores: , . : ; ...
-separadores = ".:,;¡¿!?()[]{}ªº0123456789-_"
+def leer_texto(texto):
+    """Leer texto"""
 
-palabras = {}
-anterior = ""
-# Las palabras compuestas separadas por "-" quedan unidas...
-for pal in texto.split():
-    if not pal.isalpha():
-        for char in separadores:
-            nueva_pal = pal.replace(char, "")
-            pal = nueva_pal
-            if pal.isalpha() or pal == "":
-                break
-    if pal == "":
-        continue
+    # Quitar los separadores: , . : ; ...
+    separadores = ".:,;¡¿!?()[]{}ªº0123456789-_"
 
-    if pal not in palabras:
-        palabras[pal] = {}
+    palabras = {}
+    anterior = ""
+    # Las palabras compuestas separadas por "-" quedan unidas...
+    for pal in texto.split():
+        if not pal.isalpha():
+            for char in separadores:
+                nueva_pal = pal.replace(char, "")
+                pal = nueva_pal
+                if pal.isalpha() or pal == "":
+                    break
+        if pal == "":
+            continue
 
-    if anterior == "":
+        if pal not in palabras:
+            palabras[pal] = {}
+
+        if anterior == "":
+            anterior = pal
+            continue
+
+        if pal not in palabras[anterior]:
+            palabras[anterior][pal] = 0
+        palabras[anterior][pal] += 1
+
         anterior = pal
-        continue
+    return palabras
 
-    if pal not in palabras[anterior]:
-        palabras[anterior][pal] = 0
-    palabras[anterior][pal] += 1
 
-    anterior = pal
+def elegir_palabra(palabras):
+    """Elegir una palabra para el inicio del grafo"""
+    while True:
+        print("Palabras disponibles")
+        for pal in palabras:
+            print(pal, end=" ")
+        print()
+        pal = input("Escriba la palabra por la que comenzar: ")
+        if pal in palabras:
+            return pal
 
-print(palabras)
 
-# Elegir una palabra y seguir el grafo
-print("Palabras disponibles")
-for pal in palabras:
-    print(pal, end=" ")
-print()
-inicio = input("Escriba la palabra por la que comenzar: ")
-
-print(inicio, end=" ")
-pal = inicio
-while True:
-    peso_total = 0
-    pal_sig = {}
-    for siguiente, peso in palabras[pal].items():
-        peso_total += peso
-        pal_sig[peso_total] = siguiente
-    if peso_total == 0:
-        break
-
-    peso_sig = random.randint(1, peso_total)
-    for peso in sorted(pal_sig.keys()):
-        if peso_sig <= peso:
-            pal = pal_sig[peso]
-            print(pal_sig[peso], end=" ")
+def escibir_grafo(inicio):
+    """Esribir grafo a partir de palabra de inicio"""
+    print(inicio, end=" ")
+    pal = inicio
+    while True:
+        peso_total = 0
+        pal_sig = {}
+        for siguiente, peso in palabras[pal].items():
+            peso_total += peso
+            pal_sig[peso_total] = siguiente
+        if peso_total == 0:
             break
 
-print()
+        peso_sig = random.randint(1, peso_total)
+        for peso in sorted(pal_sig.keys()):
+            if peso_sig <= peso:
+                pal = pal_sig[peso]
+                print(pal_sig[peso], end=" ")
+                break
+
+    print()
+
+
+texto = "I am, subscribed to Y-Cubed and I am 123 loving678 it."
+texto = "I am, subscribed to Y Cubed and I am 123 loving678 it."
+
+palabras = leer_texto(texto)
+inicio = elegir_palabra(palabras)
+escibir_grafo(inicio)
